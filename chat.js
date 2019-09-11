23ca10e978f51608d1bf2c1582ca488b3a5a8aa1
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-11 19:19:54 +0900

    Feat : client에 socket 메서드로 메시지 받아 화면에 출력

--- chat.js
+++ chat.js
@@ -7,7 +7,8 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 io.on("connection", socket => {
-  socket.on("newMessage", dataFromClient => {
-    console.log(dataFromClient);
+  socket.on("newMessage", msg => {
+    // console.log(msg);
+    io.emit('messageToClient', {text: msg.text})
   });
 });
