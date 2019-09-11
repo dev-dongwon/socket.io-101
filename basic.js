c15de039cf4e975a275534a8087b90478a18f993
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-11 17:04:31 +0900

    Feat : socket.io server connection

--- basic.js
+++ basic.js
@@ -2,7 +2,16 @@ const http = require("http");
 const socketio = require("socket.io");
 
 const server = http.createServer((req, res) => {
-  res.end('server connected');
+  res.end("server connected");
+});
+
+const io = socketio(server);
+
+io.on("connection", (socket, req) => {
+  socket.emit("welcome", "Welcome to websocket server");
+  socket.on("message", msg => {
+    console.log(msg);
+  });
 });
 
 server.listen(8000);
